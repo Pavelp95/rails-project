9c9b8f909fd4f00142baf077083909471a31b079
DarkGolly <geuwk65@gmail.com>
2020-09-25 12:07:01 +0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get 'project/index'
+  resources :project
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
