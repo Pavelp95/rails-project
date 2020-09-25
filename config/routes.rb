add46963425af2cceade3b9030092d81b02cdbf7
DarkGolly <geuwk65@gmail.com>
2020-09-25 12:47:42 +0500

    stage 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # get 'project/index'
-  resources :project
+  # defaults format :json do
+  #
+  # end
+  get '/projects' => 'project#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
