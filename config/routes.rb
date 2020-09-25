4a88760e8ba72007a99684324469e8e1f954a924
DarkGolly <geuwk65@gmail.com>
2020-09-25 18:34:45 +0500

    add heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   #
   # end
   get '/projects' => 'project#index'
+  root 'project#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
